85417b9d1313123bead64f1062c9c67f8bdabfa4
Daniel Isaac <danielbcbs2@gmail.com>
2018-10-05 21:17:28 +0530

    modifications

--- prototypes/flask/app.py
+++ prototypes/flask/app.py
@@ -3,7 +3,15 @@ app = Flask(__name__)
 
 @app.route('/')
 def home_page():
+    return render_template('index.html')
+
+@app.route('/about')
+def about_page():
    return render_template('about.html')
 
+@app.route('/contact')
+def contact_page():
+    return render_template('contact.html')
+    
 if __name__ == '__main__':
    app.run()
