c0866ebd91058c693de217fc13210e729591e288
Daniel Isaac <danielbcbs2@gmail.com>
2018-10-14 13:32:08 +0530

    moved files to 4sales.com folder

--- 4sale.com/app.py
+++ 4sale.com/app.py
@@ -1,6 +1,10 @@
 from flask import Flask, render_template, request, redirect, url_for
 import psycopg2
+import psycopg2.extras
 from argon2 import PasswordHasher
+from collections import defaultdict
+import map
+import greencover
 
 app = Flask(__name__)
 
@@ -14,10 +18,39 @@ def home_page():
             cur.execute("insert into users values('" + data["username"] + "','" + hashedPassword + "','" + data["firstname"] + "','" + data["lastname"] + "','" + data["emailid"] + "','" + data["phone"] + "');")
             conn.commit()
     return render_template('index.html')
+
+@app.route('/about.html')
+def about_page():
+    return render_template('about.html')
+
+@app.route('/contact.html')
+def contact_page():
+    return render_template('contact.html')
     
 @app.route('/listings_single.html')
 def listings_single():
-    return render_template('listings_single.html')
+    pid = request.args.get('id')
+    cur.execute("select * from properties where pid = " + pid + ";")
+    data = cur.fetchall()
+    cur.execute("select * from tags where pid = " + pid + ";")
+    tags = cur.fetchall()
+    print(tags)
+    address = " ".join([data[0]["address"],data[0]["city"],str(data[0]["pincode"])])
+    location = map.get_latitude_and_longitude(address)
+    print(location)
+    l = []
+    for place in map.place_types:
+        l.append(map.get_closest_places(location,place,num=2,radius=2000))
+    print(l)
+    distances = []
+    for item in l:
+        if item:
+            distances.append([map.get_distance_and_time(location,item[0][1]),map.get_distance_and_time(location,item[1][1])])
+        else:
+            distances.append([])
+    print(distances)
+    green = greencover.green_index(location["lat"],location["lng"])
+    return render_template('listings_single.html', data = data, tags = tags, proximity = l, distances = distances, green = green)
 
 @app.route('/listings.html', methods=['GET','POST'])
 def listings():
@@ -36,7 +69,20 @@ def listings():
                 return redirect(url_for('login'))
         else:
             print('Failure: invalid username')
-    return render_template('listings.html')
+    cur.execute('select * from properties')
+    data = cur.fetchall()
+    print(data)
+    cur.execute('select * from tags')
+    tags = cur.fetchall()
+    print(type(tags[0]))
+    d = defaultdict(list)
+    for tag in tags:
+        d[tag["pid"]].append(tag["tag"])
+    print(d)
+    for elem in data:
+        elem.append(d[elem[0]])
+    print(data)
+    return render_template('listings.html', data = data)
 
 @app.route('/login.html')
 def login():
@@ -52,6 +98,6 @@ def register_page():
 
 if __name__ == '__main__':
     conn = psycopg2.connect(database="forsale", user="root", password="root", host="localhost")
-    cur = conn.cursor()
+    cur = conn.cursor(cursor_factory=psycopg2.extras.DictCursor)
     ph = PasswordHasher()
     app.run()
