2555403dafa88f01d5674097538bcc655db20b1d
Daniel Isaac <danielbcbs2@gmail.com>
2018-11-19 18:57:34 +0530

    minor changes

--- 4sale.com/db/postgres_init.sql
+++ 4sale.com/db/postgres_init.sql
@@ -103,54 +103,6 @@ create table request(username varchar(30), pid int, visit varchar(3), message va
 
 create table closest_wards(ward varchar(30),ward1 varchar(30),ward2 varchar(30),ward3 varchar(30),ward4 varchar(30));
 
-/* Dummy values */
-/*
-insert into properties(title, locality, type, short_description, description, bedrooms, bathrooms, patio, address, city, pincode, cost, area, latitude, longitude) values('Beautiful Green House','Murugeshpalya','Sale','A beautiful house in bangalore region with brilliant interior design','This is an amazing house in MurugeshPalya filled with amazing features. The architecture is amazing and 10 top architects have worked on this building. It has taken 5 years to construct this house.',2,1,1,'12 S R Layout','Bangalore',560017,120300,3020,12.95100100,77.6100510);
-insert into properties(title, locality, type, short_description, description, bedrooms, bathrooms, patio, address, city, pincode, cost, area, latitude, longitude) values('Beautiful Blue House','Murugeshpalya','Sale','A beautiful house in bangalore region with brilliant interior design','This is an amazing house in MurugeshPalya filled with amazing features. The architecture is amazing and 10 top architects have worked on this building. It has taken 5 years to construct this house.',1,2,1,'12 S R Layout','Bangalore',560017,120300,3020,12.95100100,77.6100510);
-insert into properties(title, locality, type, short_description, description, bedrooms, bathrooms, patio, address, city, pincode, cost, area, latitude, longitude) values('House for sale','Murugeshpalya','Sale','A beautiful house in bangalore region with brilliant interior design','This is an amazing house in MurugeshPalya filled with amazing features. The architecture is amazing and 10 top architects have worked on this building. It has taken 5 years to construct this house.',2,1,1,'12 S R Layout','Bangalore',560017,120300,3020,12.95100100,77.6100510);
-insert into properties(title, locality, type, short_description, description, bedrooms, bathrooms, patio, address, city, pincode, cost, area, latitude, longitude) values('Villa in domlur','Murugeshpalya','Sale','A beautiful house in bangalore region with brilliant interior design','This is an amazing house in MurugeshPalya filled with amazing features. The architecture is amazing and 10 top architects have worked on this building. It has taken 5 years to construct this house.',2,1,1,'12 S R Layout','Bangalore',560017,120300,3020,12.95100100,77.6100510);
-insert into properties(title, locality, type, short_description, description, bedrooms, bathrooms, patio, address, city, pincode, cost, area, latitude, longitude) values('Green Villa','Murugeshpalya','Rent','A beautiful house in bangalore region with brilliant interior design','This is an amazing house in MurugeshPalya filled with amazing features. The architecture is amazing and 10 top architects have worked on this building. It has taken 5 years to construct this house.',2,1,1,'12 S R Layout','Bangalore',560017,120300,3020,12.95100100,77.6100510);
-insert into properties(title, locality, type, short_description, description, bedrooms, bathrooms, patio, address, city, pincode, cost, area, latitude, longitude) values('Green Bungalow','Murugeshpalya','Sale','A beautiful house in bangalore region with brilliant interior design','This is an amazing house in MurugeshPalya filled with amazing features. The architecture is amazing and 10 top architects have worked on this building. It has taken 5 years to construct this house.',2,1,1,'12 S R Layout','Bangalore',560017,120300,3020,12.95100100,77.6100510);
-insert into users values('ram_raj','ram_123','ram','raj','ram.raj@gmail.com','9445245856');
-insert into users values('ram_roy','ram_roy','ram','roy','ram.roy@gmail.com','9446585856');
-insert into users values('sadvi_s','ssadvi','sadvi','s','sadvis@gmail.com','9427955856');
-insert into users values('mani_m','mani_123','mani','m','mmani@gmail.com','9408715856');
-insert into users values('rajath','rajath_123','rajath','r','raj.raj@gmail.com','9980155856');
-insert into posts_ad values('ram_raj',1,'2000-02-01');
-insert into posts_ad values('ram_roy',2,'2001-02-03');
-insert into posts_ad values('rajath',1,'2001-02-03');
-insert into posts_ad values('mani_m',5,'2001-02-03');
-insert into posts_ad values('sadvi_s',3,'2001-02-03');
-insert into purchase values('ram_raj',1,'2001-02-03');
-insert into purchase values('mani_m',2,'2001-02-03');
-insert into purchase values('sadvi_s',3,'2001-02-03');
-insert into purchase values('ram_raj',4,'2001-02-03');
-insert into purchase values('ram_raj',5,'2001-02-03');
-insert into request values('mani_m',1,'2001-02-03');
-insert into request values('ram_raj',2,'2001-02-03');
-insert into request values('sadvi_s',3,'2001-02-03');
-insert into request values('rajath',4,'2001-02-03');
-insert into request values('ram_raj',3,'2001-02-03');
-insert into property_images values(5,'1.png');
-insert into property_images values(3,'2.png');
-insert into property_images values(2,'3.png');
-insert into property_images values(1,'4.png');
-insert into property_images values(2,'5.png');
-insert into tags values(1,'Swimming Pool');
-insert into tags values(1,'Gym');
-insert into tags values(2,'Restaurant');
-insert into tags values(3,'Play House');
-insert into tags values(3,'Buffet House');
-insert into tags values(3,'Gym');
-insert into tags values(4,'Lounge');
-insert into tags values(4,'Swimming Pool');
-insert into tags values(5,'Gym');
-insert into property_analytics values(1,4);
-insert into property_analytics values(2,9);
-insert into property_analytics values(3,7);
-insert into property_analytics values(4,2);
-insert into property_analytics values(5,1);
-*/
 \copy complaints from 'input_csv_files/complaints.csv' DELIMITER ',' CSV
 
 \copy ward_mapping from 'input_csv_files/ward-mapping.csv' DELIMITER ',' CSV
