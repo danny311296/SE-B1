8e647d8870bfb1f0ca2d985b13e853014186ddba
Daniel Isaac <danielbcbs2@gmail.com>
2018-11-19 18:55:58 +0530

    minor changes

--- 4sale.com/tests/conftest.py
+++
@@ -1,9 +0,0 @@
-import sys
-import pytest
-from flask import Flask
-
-
-@pytest.fixture
-def client():
-	app = Flask(__name__)
-	return app
--- 4sale.com/tests/test_db_utils.py
+++ 4sale.com/tests/test_db_utils.py
@@ -13,10 +13,3 @@ def create():
 
 def test_create():
 	assert create()==True
-
-def test_db_utils():
-	my_db = db("localhost","forsale","root","root")
-	conn = my_db.get_connection()
-	result = my_db.query("properties")
-	assert "select * from properties";
-
